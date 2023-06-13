9c1e8984799705e7378e5f0ffbbabfcf1aedd0b0
Mikhail <mikssur@gmail.com>
2023-06-13 19:38:29 +0300

    change instance

--- src/api/api_instance.ts
+++ src/api/api_instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: 'https://e-commerce-back-1v7z.onrender.com/api/v1/',
   headers: {
     Accept: 'application/json'
   }
